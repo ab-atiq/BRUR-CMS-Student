e901d94cd5a808f144f28682d82ddd51089a83fb
atiqur rahman <abulbasharatiqur@gmail.com>
2023-05-13 02:27:45 +0600

    image slider added

--- app/src/main/java/com/atiq/cmsstudent/ui/home/HomeFragment.java
+++ app/src/main/java/com/atiq/cmsstudent/ui/home/HomeFragment.java
@@ -9,13 +9,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.atiq.cmsstudent.R;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
-
+    private ImageSlider imageSlider;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        imageSlider = view.findViewById(R.id.image_slider);
+
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+        imageList.add(new SlideModel("https://bit.ly/2YoJ77H","This is description", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel("https://bit.ly/2BteuF2","This is description",ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel("https://bit.ly/3fLJf72","This is description",ScaleTypes.CENTER_CROP));
+        imageSlider.setImageList(imageList);
+
+        return view;
     }
 }
